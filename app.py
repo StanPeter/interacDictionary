34434f47e3516a74f1e851951f575461d8f9016b
StanPeter <stanislav.petr7@gmail.com>
2020-12-22 01:18:39 +0100

    readme improved

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ import json
 import emoji
 from difflib import get_close_matches
 
-#database for the project
-data      = json.load(open("data.json", "r"))
+
+# database for the project
+data = json.load(open("data.json", "r"))
 word_list = tuple(data.keys())
 
-#go through the database and find all matches of the input
+
+# go through the database and find all matches of the input
 def translator(word):
-    if word in data:                #for words like NASA
+    if word in data:  # for words like NASA
         w = word
-    elif word.capitalize() in data: #for words like Dublin
+    elif word.capitalize() in data:  # for words like Dublin
         w = word.capitalize()
     else:
-        w = word.lower()            #for mistakes 'River' instead of 'river'
+        w = word.lower()  # for mistakes 'River' instead of 'river'
 
     similar_word = get_close_matches(w, word_list, n=1, cutoff=0.7)
 
@@ -21,8 +23,9 @@ def translator(word):
         if w in data:
             return word_printer(w)
 
-        elif similar_word:          #if the word was badly spelled, example: 'rivver'
-            answer = input(f"Your word wasn't found, did you mean '{similar_word}' instead?  y / n \n")
+        elif similar_word:  # if the word was badly spelled, example: 'rivver'
+            answer = input(
+                f"Your word wasn't found, did you mean '{similar_word}' instead?  y / n \n")
             if answer == "y":
                 return translator(similar_word[0])
             elif answer == "n":
@@ -34,7 +37,7 @@ def translator(word):
             return "Unfortunately your word was not found in my dictionary, please check again if its correct"
 
 
-#print the final output of the found word
+# print the final output of the found word
 def word_printer(word):
     print("\n \n")
 
@@ -43,22 +46,22 @@ def word_printer(word):
 
     print("\n")
 
-#clearer communication in Terminal
+
+# clearer communication in Terminal
 def user_interface():
-    print("Hello, my name is Bob and I will be your helper throughout this translator")
+    print("Hello, my name is Bob and I will be your assistant throughout this translator")
 
     word = input("Please tell me the word you wish to find \n\n")
 
-    #repeat the programme until the user wishes
+    # repeat the programme until the user wishes
     while True:
         translator(word)
 
-        word = input("Please tell me the word you wish to find, to end the programme press 'q'   \n\n")
+        word = input(
+            "Please tell me the word you wish to find, to end the programme press 'q'   \n\n")
         if word == "q":
             print(emoji.emojize("Thank you for your time :thumbs_up:"))
             break
 
 
 user_interface()
-
-
--- readme.txt
+++ readme.txt
@@ -1,7 +1,25 @@
-An English translator, you put a word and it finds the definition of your word
+Information:
 
-Written in Python, used difflib and emoji libraries
 
-Please keep data.json in the same directory as app.py
+    An English translator, you put a word and it finds the definition of your word
 
-Create an environment, install all the packages, run app.py with Python3
+    Written in Python3, used difflib and emoji libraries
+
+    Please keep data.json in the same directory as app.py
+
+
+
+Instructions: (commands for Windows might differ)
+
+
+    1. Create a new environment                                 "  python -m venv venv  " 
+
+    2. Activate it                                              "  source venv/bin/activate  "
+
+    3. install required libraries by executing                  "  pip install -r requirements.txt  "
+
+    4. launch app.py and just follow it from there
+
+
+
+PS: programme will take some time to scrape all the pages
